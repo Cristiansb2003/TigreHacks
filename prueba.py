d64ba775d2d151ba59b5bffb7940b31da280f60b
Franco <diegoantoniolopezruelas@gmail.com>
2023-05-27 04:54:18 -0600

    Para juntar todos los csv

--- prueba.py
+++ prueba.py
@@ -1,25 +1,43 @@
 import requests
+import csv
+from datetime import datetime
 
 # The API endpoint
 municipios = ['apodaca','garcia','general-escobedo','guadalupe','juarez','monterrey','san-nicolas-de-los-garza','santa-catarina','santiago']
-reportes = []
 
 for m in municipios:
-  url = "https://nuevoleon.opendatasoft.com/api/records/1.0/search/?dataset=indices-de-estadisticas-de-accidentes-viales-" + m + "&rows=1"
+  url = "https://nuevoleon.opendatasoft.com/api/records/1.0/search/?dataset=indices-de-estadisticas-de-accidentes-viales-" + m + "&rows=10000"
 
   # A GET request to the API
   response = requests.get(url)
   # Print the response
   response_json = response.json()
+  error = 0
 
   for i in response_json['records']:
-    reportes.append({
-        "folio": (i['fields']['folio']),
-        "fecha": (i['fields']['fecha']),
-        "dia": (i['fields']['dia']),
-        "hora": (i['fields']['hora']),
-        "tipo_de_accidente": (i['fields']['tipo_de_accidente']),
-        "latitud": (i['fields']['georreferencia'][0]),
-        "longitud": (i['fields']['georreferencia'][1]),
-        "municipio": m
-    })
+    try:
+      fecha = i['fields']['fecha']
+      date_object = datetime.strptime(fecha, '%Y-%m-%d').date()
+      day_object = date_object.strftime('%A')
+
+      hora = i['fields']['hora']
+      time_object = datetime.strptime(hora, '%H:%M:%S').time()
+
+      reporte = [
+        i['fields']['folio'],
+        date_object,
+        day_object,
+        time_object,
+        i['fields']['tipo_de_accidente'],
+        i['fields']['georreferencia'][0],
+        i['fields']['georreferencia'][1],
+        m
+      ]
+    except:
+      error = error + 1
+    else:
+      with open(r'reportes.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(reporte)
+    
+print(error)
